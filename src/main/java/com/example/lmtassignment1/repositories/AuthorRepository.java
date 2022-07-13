ed43037cb0166cf64f8d144654a3027892fd8ffb
Lea_Cook <lea@apothesource.com>
2022-07-13 15:11:40 -0400

    first commit

---
+++ src/main/java/com/example/lmtassignment1/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.example.lmtassignment1.repositories;
+
+import com.example.lmtassignment1.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+}
---
+++ src/main/java/com/example/lmtassignment1/repositories/PublisherRepository.java
@@ -0,0 +1,6 @@
+package com.example.lmtassignment1.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository {
+}
